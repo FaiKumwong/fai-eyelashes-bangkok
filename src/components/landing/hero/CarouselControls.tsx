dcd79fb6ee76d3c4198bb0fcf0ccbc7369b6e0de
FaiKumwong <kumwong1999@gmail.com>
2024-12-29 21:58:45 +0700

    Update CarouselControls.tsx

--- src/components/landing/hero/CarouselControls.tsx
+++ src/components/landing/hero/CarouselControls.tsx
@@ -10,8 +10,7 @@ const CarouselControls = ({ onPrevSlide, onNextSlide }: CarouselControlsProps) =
   return (
     <>
       <button
-        className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white/20 p-2 rounded-full hover:bg-white/40 transition-colors"
-        onClick={onPrevSlide}
+
       >
         <ChevronLeft className="w-6 h-6 text-white" />
       </button>
