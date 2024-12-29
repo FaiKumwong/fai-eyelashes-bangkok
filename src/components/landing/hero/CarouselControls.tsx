ffd5260db77a2774fc080d9f21b62c0ce48627c6
FaiKumwong <kumwong1999@gmail.com>
2024-12-29 22:01:54 +0700

    Update CarouselControls.tsx add hidden

--- src/components/landing/hero/CarouselControls.tsx
+++ src/components/landing/hero/CarouselControls.tsx
@@ -10,12 +10,13 @@ const CarouselControls = ({ onPrevSlide, onNextSlide }: CarouselControlsProps) =
   return (
     <>
       <button
-
+        className="hidden absolute left-4 top-1/2 transform -translate-y-1/2 bg-white/20 p-2 rounded-full hover:bg-white/40 transition-colors"
+        onClick={onPrevSlide}
       >
         <ChevronLeft className="w-6 h-6 text-white" />
       </button>
       <button
-        className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white/20 p-2 rounded-full hover:bg-white/40 transition-colors"
+        className="hidden absolute right-4 top-1/2 transform -translate-y-1/2 bg-white/20 p-2 rounded-full hover:bg-white/40 transition-colors"
         onClick={onNextSlide}
       >
         <ChevronRight className="w-6 h-6 text-white" />
